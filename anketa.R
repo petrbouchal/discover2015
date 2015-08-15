90d9e9945fcd0b552af7825d9beaef028eb2979d
petrbouchal <pbouchal@gmail.com>
2015-08-15 15:59:35 +0200

    better income data v ankete

--- anketa.R
+++ anketa.R
@@ -5,13 +5,35 @@ dsc <- read_excel("~/Documents/Work/Discover/Discover 2015/Workshop nerovnosti/p
 table(is.na(dsc$hplat_otec_eur))
 table(is.na(dsc$hplat_matka_eur))
 
+medianzeny <- 20856
+medianmuzi <- 24909
+median <- 23072
+
+pc10muzi <- 12994
+pc10zeny <- 11685
+pc10 <- 12173
+
+pc90muzi <- 42297
+pc90zeny <- 36081
+pc90 <- 42346
+
+euro <- 25
+
+## decily: http://www.statistikaamy.cz/2015/04/prumerna-mzda-a-median/
+
+
 table(!is.na(dsc$hplat_matka_eur) & !is.na(dsc$hplat_otec_eur))
 
 dsc$hp_rodina <- dsc$hplat_otec_eur + dsc$hplat_matka_eur
 
-dsc$otec_pod10pc <- ifelse(dsc$hplat_otec_eur < 760, 1, 0)
-dsc$matka_pod10pc <- ifelse(dsc$hplat_matka_eur < 760, 1, 0)
+dsc$otec_nad50pc <- ifelse(dsc$hplat_otec_eur > medianmuzi/euro, 1, 0)
+dsc$matka_nad50pc <- ifelse(dsc$hplat_matka_eur > medianzeny/euro, 1, 0)
 
+dsc$otec_pod10pc <- ifelse(dsc$hplat_otec_eur < pc10muzi/euro, 1, 0)
+dsc$matka_pod10pc <- ifelse(dsc$hplat_matka_eur < pc10zeny/euro, 1, 0)
+
+dsc$otec_nad90pc <- ifelse(dsc$hplat_otec_eur > pc90muzi/euro, 1, 0)
+dsc$matka_nad90pc <- ifelse(dsc$hplat_matka_eur > pc90zeny/euro, 1, 0)
 
 mean(dsc$vs_otec, na.rm = T)
 mean(dsc$vs_matka, na.rm = T)
@@ -24,6 +46,8 @@ median(dsc$hplat_otec_eur, na.rm = T)
 median(dsc$hplat_matka_eur, na.rm = T)
 median(dsc$hp_rodina, na.rm = T)
 
+mean(dsc$otec_nad50pc, na.rm = T)
+mean(dsc$matka_nad50pc, na.rm = T)
 mean(dsc$otec_nad90pc, na.rm = T)
 mean(dsc$matka_nad90pc, na.rm = T)
 mean(dsc$otec_pod10pc, na.rm = T)
